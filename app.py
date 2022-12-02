c3b7a73237049761b6a099c86a573e1aea2daa62
Marcos V. Shiuuri <98934927+MarcosShiuuri@users.noreply.github.com>
2022-12-02 16:07:35 -0300

    Finished project

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ db.init_app(app)
 
 class Expenses(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), nullable=False)
-    local = db.Column(db.String(32), nullable=False)
-    payment = db.Column(db.String(12), nullable=False)
-    value = db.Column(db.Float(5), nullable=False)
-    total = db.Column(db.Float(6), nullable=False)
+    name = db.Column(db.String(200), nullable=False)
+    local = db.Column(db.String(200), nullable=False)
+    payment = db.Column(db.String(200), nullable=False)
+    value = db.Column(db.Float(200), nullable=False)
     def __repr__(self):
         return '<Expenses %r>' % self.id
 with app.app_context():
@@ -25,8 +24,7 @@ def index():
         exp_local = rq.form.get('exp_local')
         exp_pay = rq.form.get('exp_payment')
         exp_value = rq.form.get('exp_value')
-        exp_total = 0
-        new_expense = Expenses(name=exp_name, local=exp_local, payment=exp_pay, value=exp_value, total=exp_total)
+        new_expense = Expenses(name=exp_name, local=exp_local, payment=exp_pay, value=exp_value)
         try:
             db.session.add(new_expense)
             db.session.commit()
@@ -36,4 +34,18 @@ def index():
             return rt('error.html', error=error)
     else:
         current_expenses = Expenses.query.order_by(Expenses.id).all()
-        return rt('index.html', exp=current_expenses)
+        total = 0
+        for x in current_expenses:
+            total += x.value
+        return rt('index.html', exp=current_expenses, total=float(round(total,2)))
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    expense_delete = Expenses.query.get_or_404(id)
+    try:
+        db.session.delete(expense_delete)
+        db.session.commit()
+        return rd('/')
+    except:
+        error = 'Could not delete, please try again.'
+        return rt('error.html', error=error)
